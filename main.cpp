07386b0453bdc5ac217ceb1c9276ae1da8c413b3
fpringle <freddyjepringle@gmail.com>
2022-10-25 12:57:58 +0200

    update copyright

--- main.cpp
+++ main.cpp
@@ -1,4 +1,10 @@
-/* Copyright 2020 Freddy Pringle */
+/*
+Copyright (c) 2022, Frederick Pringle
+All rights reserved.
+
+This source code is licensed under the BSD-style license found in the
+LICENSE file in the root directory of this source tree.
+*/
 #include "interface.h"
 
 int main() {
